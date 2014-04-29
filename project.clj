3020012460388859e259d0d711b9bd686d1ec100
Sidhant Godiwala <grinnbearit@liara.local>
2014-04-29 16:46:34 +0530

    bump libs

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject crawler "0.1.0"
   :description "website crawler"
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [enlive "1.1.5"]
-                 [http-kit "2.1.13"]])
+                 [http-kit "2.1.16"]])
