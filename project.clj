7bdac10410c6022922c6733f35164ab28f958c8b
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2014-10-08 12:11:24 +0530

    cheshire as a dev dependency

--- project.clj
+++ project.clj
@@ -5,4 +5,5 @@
                  [http-kit "2.1.16"]
                  [hiccup "1.0.5"]]
   :profiles {:dev {:plugins [[lein-midje "3.1.3"]]
-                   :dependencies [[midje "1.6.3"]]}})
+                   :dependencies [[midje "1.6.3"]
+                                  [cheshire "5.3.1"]]}})
